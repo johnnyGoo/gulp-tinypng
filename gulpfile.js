fc858ea44cc201f0e8a5b1ffdd16b67922a17f6a
Johnny <157781745@qq.com>
2017-12-26 01:14:14 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var gulp = require('gulp');
+var tinify = require('./index');
+var moment = require('moment');
+
+var keys = [];
+
+keys.push('Aqaxt5h663fpc84PKpFcUbDVWTSIhuax');
+keys.push('dumK3nGimIxRaxyH5CjU1z_ykuc8Y4ow');
+keys.push('GnD_B7U9ceTjsQS6QxpJWbDKHsryzpxQ');
+keys.push('y0sXlJALE1wu6ueRyXsHbiS1GoJf5XmT');
+
+
+
+var fs = require('fs');
+var file = 'cache.json';
+var fileData;
+const MAX_COUNT_PER_KEY = 500;
+function initData() {
+    return {keys: [], currentKey: 0, datetime: moment().subtract(-1, 'months').format()};
+}
+
+try {
+    fileData = JSON.parse(fs.readFileSync(file));
+} catch (err) {
+    fileData = initData();
+}
+if (moment().unix() > moment(fileData.datetime).unix()) {
+    //到期后重置数据
+    fileData = initData();
+}
+
+function checkKey(fileData, key) {
+    for (var i = 0; i < fileData.keys.length; i++) {
+        var keyobj = fileData.keys[i];
+        if (keyobj.key === key) {
+            return true;
+        }
+    }
+    return false;
+}
+
+for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    if (checkKey(fileData, key) === false) {
+        fileData.keys.push({key: key, count: MAX_COUNT_PER_KEY});
+    }
+}
+
+
+function getKey() {
+    var keyobj = fileData.keys[fileData.currentKey];
+    if (keyobj.count > 0) {
+        keyobj.count = keyobj.count - 1;
+        return keyobj.key;
+    } else {
+        if (fileData.currentKey < fileData.keys.length - 1) {
+            fileData.currentKey++;
+            return getKey();
+        } else {
+            console.log('key empty');
+            return false;
+        }
+
+    }
+}
+
+gulp.task('tinify', function () {
+    gulp.src('./img/*')
+        .pipe(tinify(getKey))
+        .pipe(gulp.dest('dist'))
+        .on('end', function () {
+            //处理完成之后 保存缓存文件
+           // console.log(fileData);
+            fs.writeFile(file, JSON.stringify(fileData),function (err) {
+                if(err) throw err;
+               // console.log('saved cache file')
+                
+            });
+        });
+});
+
+
+gulp.task('default', ['tinify']);
---
+++ index.js
@@ -0,0 +1,71 @@
+/**
+ * Created by johnny on 2017/12/25.
+ */
+var fs          = require('fs'),
+    path        = require('path'),
+    through     = require('through2-concurrent'),
+    gutil       = require('gulp-util'),
+    tinify      = require('tinify'),
+    prettyBytes = require('pretty-bytes'),
+    PluginError = gutil.PluginError;
+
+const PLUGIN_NAME = 'gulp-tinyimg';
+
+function tinypng(apiKey, file) {
+    var validExtensions = ['.png', '.jpg', '.jpeg'];
+
+    if(!apiKey) {
+        throw new PluginError(PLUGIN_NAME, 'We can\'t upload images without your API Key');
+    }
+    return through.obj(function(file, enc, cb) {
+        if(file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+
+        if(file.isStream()) {
+            cb(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            return;
+        }
+
+        if(validExtensions.indexOf(path.extname(file.path)) === -1) {
+            gutil.log(PLUGIN_NAME + ': Skipping unsupported image ' + file.path);
+            cb(null, file);
+            return;
+        }
+        var prevLength = file.contents.length;
+        function tiny(contents,retry) {
+            tinify.key=apiKey();
+            tinify.fromBuffer(contents).toBuffer(function(err, resultData) {
+                if (err instanceof tinify.AccountError) {
+                    console.log("The error message is: " + err.message);
+                    if(retry){
+                    tiny(contents,false);
+                    }else{
+                        cb(null, file);
+                    }
+                    // Verify your API key and account limit.
+                }  else {
+                    // Something else went wrong, unrelated to the Tinify API.
+
+                    if(err){
+                        cb(null, file);
+                    }else{
+                        fs.unlink(file.path);
+                        file.contents = resultData;
+                        gutil.log('gulp-tinypng: ', gutil.colors.green('✔ ') + file.relative + ' (saved ' +
+                            prettyBytes(prevLength - resultData.length) + ' - ' + ((1 - resultData.length / prevLength) * 100).toFixed(0) + '%)');
+                        cb(null, file);
+                        return true;
+                    }
+                }
+                return false;
+            })
+        }
+        tiny(file.contents,true);
+
+    });
+}
+// Exporting the plugin main function
+module.exports = tinypng;
