0c3b907efdf122c359176df4f1492258572fa2d2
Johnny <157781745@qq.com>
2018-02-06 19:57:59 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 var file = 'cache.json';
 var fileData;
 const MAX_COUNT_PER_KEY = 500;
+var b='cc';
 function initData() {
     return {keys: [], currentKey: 0, datetime: moment().subtract(-1, 'months').format()};
 }
